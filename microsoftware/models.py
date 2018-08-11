78b451d449f6f72f30275994ce479e8d99f8c62d
will-lzx <will-liu@live.com>
2018-08-11 11:35:58 +0800

    refine db

--- microsoftware/models.py
+++ microsoftware/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 # Create your models here.
 
-
-class Customer(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
-    openid = models.CharField(max_length=200)
-    createtime = models.DateTimeField()
-    updatetime = models.DateTimeField(auto_now=True)
+#
+# class Customer(models.Model):
+#     id = models.IntegerField(primary_key=True, auto_created=True)
+#     openid = models.CharField(max_length=200)
+#     createtime = models.DateTimeField()
+#     updatetime = models.DateTimeField(auto_now=True)
 
 
 class Production(models.Model):
